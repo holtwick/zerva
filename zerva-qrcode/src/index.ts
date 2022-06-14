7b3baa76c7f0e64b4b17d5dbca2fa65dc4d0a47a
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-06-14 16:26:01 +0200

    Fix QRCode

--- zerva-qrcode/src/index.ts
+++ zerva-qrcode/src/index.ts
@@ -2,19 +2,22 @@
 // https://github.com/svitejs/vite-plugin-qrcode (MIT)
 
 import { on } from "@zerva/core"
+import "@zerva/http"
 import type { Server } from "http"
 import { AddressInfo } from "net"
 import os from "os"
 import qr from "qrcode-terminal"
 import { Logger } from "zeed"
-import "@zerva/http"
 
 const log = Logger("zerva:qrcode")
 
 function logQrcode(http: Server) {
-  const networkUrls = getNetworkUrls(http)
-  if (networkUrls.length === 0) return
-  console.info("  Visit page on mobile:")
+  const networkUrls = getNetworkUrls(http) 
+  if (networkUrls.length === 0) {
+    console.info("Zerva: No external server IP available.")
+    return
+  }
+  console.info("Zerva: Visit page on mobile using QRCode")
   for (const url of networkUrls) {
     qr.generate(url, { small: true }, (result) => {
       console.info(`\n  ${cyan(url)}\n  ${result.replace(/\n/g, "\n  ")}\n`)
@@ -28,19 +31,21 @@ function cyan(str: string): string {
 
 // // Referenced from https://github.com/vitejs/vite/blob/77447496704e61cdb68b5788d8d79f19a2d895f1/packages/vite/src/node/logger.ts#L143
 function getNetworkUrls(server: Server): string[] {
-  const address = server?.address()
+  const address = server?.address()  
   if (!isAddressInfo(address)) return []
   // const hostname = resolveHostname(server.config.server.host)
   // if (hostname.host === "127.0.0.1") return []
   const protocol = "http"
   const port = address.port
+
   return Object.values(os.networkInterfaces())
     .flatMap((nInterface) => nInterface ?? [])
     .filter(
       (detail) =>
         detail &&
         detail.address &&
-        detail.family === "IPv4" &&
+        // @ts-ignore
+        (detail.family === "IPv4" || detail.family === 4) &&
         !detail.address.includes("127.0.0.1")
     )
     .map((detail) => `${protocol}://${detail.address}:${port}`)
@@ -86,9 +91,8 @@ function isAddressInfo(v: any): v is AddressInfo {
 //   name: string
 // }
 
-export function useQrCode() {
-  log("qrcode")
-  on("httpRunning", ({ http }) => {
+export function useQrCode() { 
+  on("httpRunning", ({ http }) => {    
     logQrcode(http)
   })
 }
