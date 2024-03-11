f1fd5e0008a0d165f886b94e0cf0a76ddf81a081
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-03-11 10:33:26 +0100

    fix: vitest and tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,18 @@
-/* eslint-disable node/prefer-global/process */
 /// <reference types="vitest" />
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import process from 'node:process'
 
 export default defineConfig({
   test: {
-    // setupFiles: ['vitest-setup.ts'],
     include: ['**/*.{client,test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', '_archive', '*/_archive/*'],
-    // snapshotFormat: {
-    //   printBasicPrototype: true,
-    // },
-    // root: './src',
     globals: true,
-    threads: false,
-    // singleThread: true,
-    // watch: true,
-    // isolate: false,
+    poolOptions: {
+      threads:  {
+        singleThread: true
+      }
+    },
     alias: {
       '@/': `${resolve(process.cwd(), 'src')}/`,
     },
