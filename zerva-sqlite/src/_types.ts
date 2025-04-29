29518fc295c594955276c2234831290e44e55db7
Dirk Holtwick <dirk.holtwick@gmail.com>
2025-04-29 16:12:34 +0200

    fix: update dependencies

--- zerva-sqlite/src/_types.ts
+++ zerva-sqlite/src/_types.ts
@@ -3,7 +3,7 @@ import { isNumber, isString } from 'zeed'
 declare module 'zeed' {
   export interface TypeProps {
     /** Type to be used as SQLite field. See https://www.sqlite.org/datatype3.html#affinity_name_examples */
-    fieldType: 'integer' | 'real' | 'text' | 'numeric' | 'blob'
+    fieldType?: 'integer' | 'real' | 'text' | 'numeric' | 'blob'
 
     /** Index column */
     // fieldIndex?: boolean
